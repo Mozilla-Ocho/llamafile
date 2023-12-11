c2a7a5e8f2b3f9dad8f1c77ceb8833b32493c603
Jōshin <git@wholezero.org>
2023-12-11 22:19:59 +0000

    Code to the specific arguments GGML passes

--- llama.cpp/naive-gemm.cu
+++ llama.cpp/naive-gemm.cu
@@ -2,45 +2,36 @@
 #include <cuda_fp16.h>
 #include <cublas_v2.h>
 
-#define MULZERO(X, Y) (fabs((X)) > 0 ? (X) * (Y) : 0.0)
-#define READ0(A, trans, ld, i, j) \
-  (((trans) == CUBLAS_OP_N) ? (A)[(i) + (j) * (ld)] : (A)[(j) + (i) * (ld)])
-#define READ(A, type, trans, ld, i, j) \
-  ((type) == CUDA_R_16F                                         \
-   ? __half2float(READ0((half *)(A), (trans), (ld), (i), (j)))  \
-   : READ0((float *)(A), (trans), (ld), (i), (j)))
+#define READ(A, trans, ld, i, j) \
+  __half2float(((trans) == CUBLAS_OP_N) \
+                 ? (A)[(i) + (j) * (ld)] : (A)[(j) + (i) * (ld)])
 
-static __device__ __forceinline__ void matmul(cublasOperation_t transa,
-                                              cublasOperation_t transb,
-                                              int m, int n, int k,
-                                              float               alpha,
-                                              const void          *A,
-                                              cublasDataType_t    Atype,
-                                              int lda,
-                                              const void          *B,
-                                              cublasDataType_t    Btype,
-                                              int ldb,
-                                              float               beta,
-                                              void                *C,
-                                              cublasDataType_t    Ctype,
-                                              int ldc) {
+static __device__ __forceinline__ void matmul(int m, int n, int k,
+                                              const half *A, int lda,
+                                              const half *B, int ldb,
+                                              half       *C, int ldc) {
   for (int i = 0; i < m; ++i) {
     for (int j = 0; j < n; ++j) {
       float sum = 0.0;
+      half *cptr = C + i + j * ldc;
       for (int l = 0; l < k; ++l) {
-        sum += READ(A, Atype, transa, lda, i, l) *
-               READ(B, Btype, transb, ldb, l, j);
+        sum += READ(A, CUBLAS_OP_T, lda, i, l) *
+               READ(B, CUBLAS_OP_N, ldb, l, j);
       }
-      if (Ctype == CUDA_R_16F) {
-        half *cptr = (half *)C + i + ldc * j;
-        *cptr = __float2half(MULZERO(alpha, sum) +
-                             MULZERO(beta, __half2float(*cptr)));
-      } else {
-        float *cptr = (float *)C + i + ldc * j;
-        *cptr = MULZERO(alpha, sum) + MULZERO(beta, *cptr);
-      }
+      *cptr = __float2half(sum);
     }
   }
+}
+
+static bool check_args(cublasOperation_t transa, cublasOperation_t transb,
+                       const void *pAlpha, cudaDataType_t Atype,
+                       cudaDataType_t Btype, const void *pBeta,
+                       cudaDataType_t Ctype, cublasComputeType_t computeType) {
+  return transa == CUBLAS_OP_T && transb == CUBLAS_OP_N &&
+    Atype == CUDA_R_16F && Btype == CUDA_R_16F && Ctype == CUDA_R_16F &&
+    computeType == CUBLAS_COMPUTE_16F &&
+    __half2float(*(half *)pAlpha) == 1.0f &&
+    __half2float(*(half *)pBeta) == 0.0f;
 }
 
 // https://docs.nvidia.com/cuda/cublas/index.html#cublasgemmstridedbatchedex
@@ -55,37 +46,23 @@ static __device__ __forceinline__ void matmul(cublasOperation_t transa,
    ? (const void *)STRIDE0((const half *)(A), (i), (stride))  \
    : (const void *)STRIDE0((const float *)(A), (i), (stride)))
 
-static __global__ void cublasGSBE_entry(cublasOperation_t transa,
-                                        cublasOperation_t transb,
-                                        int m, int n, int k,
-                                        float           alpha,
-                                        const void      *A,
-                                        cudaDataType_t  Atype,
+static __global__ void cublasGSBE_entry(int m, int n, int k,
+                                        const half      *A,
                                         int             lda,
                                         long long int   strideA,
-                                        const void      *B,
-                                        cudaDataType_t  Btype,
+                                        const half      *B,
                                         int             ldb,
                                         long long int   strideB,
-                                        float           beta,
-                                        void            *C,
-                                        cudaDataType_t  Ctype,
+                                        half            *C,
                                         int             ldc,
                                         long long int   strideC,
                                         int batchCount) {
   int x = blockIdx.x * blockDim.x + threadIdx.x;
   int jump = blockDim.x * gridDim.x;
 
-  const void *A_x;
-  const void *B_x;
-  void *C_x;
-
   for (; x < batchCount; x += jump) {
-    A_x = STRIDE_CONST(A, Atype, x, strideA);
-    B_x = STRIDE_CONST(B, Btype, x, strideB);
-    C_x = STRIDE(C, Ctype, x, strideC);
-    matmul(transa, transb, m, n, k, alpha, A_x, Atype, lda, B_x, Btype, ldb,
-           beta, C_x, Ctype, ldc);
+    matmul(m, n, k, A + x * strideA, lda, B + x * strideB, ldb, C + x * strideC,
+           ldc);
   }
 }
 
@@ -110,26 +87,9 @@ cublasStatus_t cublasGemmStridedBatchedEx(cublasHandle_t handle,
                                           int batchCount,
                                           cublasComputeType_t computeType,
                                           cublasGemmAlgo_t algo) {
-  if ((Atype != CUDA_R_16F && Atype != CUDA_R_32F) ||
-      (Btype != CUDA_R_16F && Btype != CUDA_R_32F) ||
-      (Ctype != CUDA_R_16F && Ctype != CUDA_R_32F) ||
-      (transa != CUBLAS_OP_N && transa != CUBLAS_OP_T) ||
-      (transb != CUBLAS_OP_N && transb != CUBLAS_OP_T)) {
+  if (!check_args(transa, transb, pAlpha, Atype, Btype, pBeta, Ctype,
+                  computeType)) {
     return CUBLAS_STATUS_NOT_SUPPORTED;
-  }
-
-  float alpha, beta;
-  switch (computeType) {
-    case CUBLAS_COMPUTE_16F:
-      beta = __half2float(*(half *)pBeta);
-      alpha = __half2float(*(half *)pAlpha);
-      break;
-    case CUBLAS_COMPUTE_32F:
-      beta = *(float *)pBeta;
-      alpha = *(float *)pAlpha;
-      break;
-    default:
-      return CUBLAS_STATUS_NOT_SUPPORTED;
   }
 
   cudaStream_t stream;
@@ -144,8 +104,8 @@ cublasStatus_t cublasGemmStridedBatchedEx(cublasHandle_t handle,
 
   // call the entry function
   cublasGSBE_entry<<<maxblocks, maxthreads, 0, stream>>>(
-      transa, transb, m, n, k, alpha, A, Atype, lda, strideA, B, Btype, ldb,
-      strideB, beta, C, Ctype, ldc, strideC, batchCount);
+      m, n, k, (const half*)A, lda, strideA, (const half*)B, ldb, strideB,
+      (half *)C, ldc, strideC, batchCount);
 
   return CUBLAS_STATUS_SUCCESS;
 }
