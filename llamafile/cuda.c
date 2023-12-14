41f39480afcd1e7e1a2b2a2ec2e5273b32d5b7d2
Jōshin <git@wholezero.org>
2023-12-13 22:00:22 -0500

    Fix irrelevant bounds check in cuda.c
    May as well be `strcpy` and `strcat` in this case, but having the wrong size looks weird.

--- llamafile/cuda.c
+++ llamafile/cuda.c
@@ -394,8 +394,8 @@ static bool ExtractCudaDso(char dso[static PATH_MAX]) {
 
     // see if prebuilt dso is bundled in zip assets
     char zip[80];
-    strlcpy(zip, "/zip/ggml-cuda.", PATH_MAX);
-    strlcat(zip, GetDsoExtension(), PATH_MAX);
+    strlcpy(zip, "/zip/ggml-cuda.", sizeof(zip));
+    strlcat(zip, GetDsoExtension(), sizeof(zip));
     if (!FileExists(zip)) {
         tinyprint(2, "prebuilt binary ", zip, " not found\n", NULL);
         return false;
