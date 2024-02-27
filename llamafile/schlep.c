6e47b0b4b12d34c1a5ef265f6f6d9323917fb7a6
Justine Tunney <jtunney@mozilla.com>
2024-02-27 02:00:03 -0800

    Don't show loading progress with --log_disable

--- llamafile/schlep.c
+++ llamafile/schlep.c
@@ -15,14 +15,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <cosmo.h>
-#include <stdio.h>
-#include <errno.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <sys/auxv.h>
-#include <stdatomic.h>
 #include "llama.cpp/ggml.h"
+#include "llamafile/log.h"
+#include <cosmo.h>
+#include <errno.h>
+#include <pthread.h>
+#include <stdatomic.h>
+#include <stdio.h>
+#include <sys/auxv.h>
+#include <unistd.h>
 
 #define FPS 24
 #define THREADS 4
@@ -54,8 +55,10 @@ static void *PageFaulter(void *arg) {
 static void FormatPercent(char sbuf[static 8], double x) {
     char *p = sbuf;
     int n = x * 100000.5;
-    if (n >= 100000) *p++ = '0' + n / 100000 % 10;
-    if (n >= 10000) *p++ = '0' + n / 10000 % 10;
+    if (n >= 100000)
+        *p++ = '0' + n / 100000 % 10;
+    if (n >= 10000)
+        *p++ = '0' + n / 10000 % 10;
     *p++ = '0' + n / 1000 % 10;
     *p++ = '.';
     *p++ = '0' + n / 100 % 10;
@@ -68,6 +71,11 @@ static void FormatPercent(char sbuf[static 8], double x) {
  * Loads memory off disk while reporting progress.
  */
 void llamafile_schlep(const void *data, size_t size) {
+
+    // don't bother if logging is disabled
+    if (FLAG_log_disable) {
+        return;
+    }
 
     // don't bother if memory is small
     if (size < 128 * 1024 * 1024) {
@@ -109,7 +117,8 @@ void llamafile_schlep(const void *data, size_t size) {
     for (;;) {
         char percent[8];
         long count = atomic_load_explicit(&faults, memory_order_acquire);
-        if (count == pages) break;
+        if (count == pages)
+            break;
         FormatPercent(percent, (double)count / pages);
         tinyprint(2, "\rmemory map ", percent, "% loaded...\033[K", NULL);
         usleep(1. / FPS * 1e6);
