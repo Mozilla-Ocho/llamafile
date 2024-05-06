56fc2cd9879175c5f19c8bb4ae419d1f214d4222
Jōshin <git@wholezero.org>
2024-05-06 11:27:52 -0700

    vim spells the c++ filetype 'cpp'
    Accomplished via:
    
        rg 'ft=c\+\+' -l0 | xargs -0 sed -i '' 's/ft=c++/ft=cpp/'

--- llama.cpp/ggml-cuda.cu
+++ llama.cpp/ggml-cuda.cu
@@ -1,5 +1,5 @@
 // -*- mode:c++;indent-tabs-mode:nil;c-basic-offset:4;coding:utf-8 -*-
-// vi: set et ft=c++ ts=4 sts=4 sw=4 fenc=utf-8 :vi
+// vi: set et ft=cpp ts=4 sts=4 sw=4 fenc=utf-8 :vi
 
 #include <algorithm>
 #include <stdarg.h>
