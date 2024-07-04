1601118bde65ff4533e39fb75295014932a77ddf
Justine Tunney <jtunney@mozilla.com>
2024-07-04 14:55:31 -0700

    Revert "Disable warmup"
    This reverts commit 21a30bed3dbcf91a158376345699014d282c42c7.
    
    See #485

--- llama.cpp/common.h
+++ llama.cpp/common.h
@@ -171,7 +171,7 @@ struct gpt_params {
     bool infill            = false; // use infill mode
     bool dump_kv_cache     = false; // dump the KV cache contents for debugging purposes
     bool no_kv_offload     = false; // disable KV offloading
-    bool warmup            = false; // warmup run
+    bool warmup            = true;  // warmup run
     bool check_tensors     = false; // validate tensor data
 
     std::string cache_type_k = "f16"; // KV cache data type for the K
