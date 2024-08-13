2f46da6e03416ef9597615526d3c6faf7404d66e
Okohedeki <okohedeki@gmail.com>
2024-08-13 00:51:15 -0700

    Update BUILD.mk
    Closing this one

--- stb/BUILD.mk
+++ stb/BUILD.mk
@@ -3,11 +3,7 @@
 
 PKGS += STB
 
-STB_FILES := $(wildcard stb/*)
-STB_HDRS = $(filter %.h,$(STB_FILES))
-STB_INCS = $(filter %.inc,$(STB_FILES))
-STB_SRCS = $(filter %.c,$(STB_FILES))
-STB_OBJS = $(STB_SRCS:%.c=o/$(MODE)/%.o)
+
 
 o/$(MODE)/stb/stb.a: $(STB_OBJS)
 
