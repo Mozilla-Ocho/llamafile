dd75ecebf3860f6364eee4d54ffef23f463e2aca
CJ Pais <cj@cjpais.com>
2025-03-10 12:05:46 -0700

    Release llamafile v0.9.1

--- llamafile/rocm.sh
+++ llamafile/rocm.sh
@@ -5,6 +5,27 @@
 # NVCUDA DLLs are provided by the installation of the windows GPU
 # driver on a Windows system that has a CUDA-capable GPU installed.
 
+TMP=$(mktemp -d) || exit
+
+cp llama.cpp/ggml-cuda.cu \
+   llama.cpp/ggml-cuda.h \
+   llama.cpp/ggml-impl.h \
+   llama.cpp/ggml-alloc.h \
+   llama.cpp/ggml-common.h \
+   llama.cpp/ggml-backend.h \
+   llama.cpp/ggml-backend-impl.h \
+   llama.cpp/ggml.h \
+   llamafile/tinyblas.h \
+   llamafile/tinyblas.cu \
+   llamafile/llamafile.h \
+   llamafile/rocm.bat \
+   llamafile/rocm.sh \
+   llamafile/cuda.bat \
+   llamafile/cuda.sh \
+   "$TMP" || exit
+
+cd "$TMP"
+
 hipcc \
   -O2 \
   -fPIC \
@@ -23,5 +44,5 @@ hipcc \
   -DGGML_MINIMIZE_CODE_SIZE=1 \
   -DGGML_CUDA_PEER_MAX_BATCH_SIZE=128 \
   --amdgpu-target=gfx1100,gfx1031,gfx1030,gfx1032,gfx906,gfx1101,gfx1102,gfx1103 \
-  -o ggml-rocm.so \
+  -o ~/ggml-rocm.so \
   ggml-cuda.cu
